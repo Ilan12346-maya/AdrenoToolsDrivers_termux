8bc18c5b2fb74e3eac7e7947466c17cbb7b9edcb
Ilan12346-maya <103835810+Ilan12346-maya@users.noreply.github.com>
2026-02-15 12:43:59 +0100

    Update info.sh

--- info.sh
+++ info.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-
 echo "Vulkan:"
- vulkaninfo 2>/dev/null | grep -m 1 "deviceName" | cut -d "=" -f2 | xargs
-
+vulkaninfo 2>/dev/null | grep -m 1 "deviceName" | cut -d "=" -f2 | xargs
 vulkaninfo 2>/dev/null | grep -m 1 "driverVersion" | cut -d "=" -f2 | xargs
